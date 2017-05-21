01d16fe7069d0f9b7702b07d39e84390924f63dd
vhoyer <vhoyer@live.com>
2017-05-20 22:05:09 -0300

    Added limited scroll
    Signed-off-by: vhoyer <vhoyer@live.com>

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var maxheight = $("#thisis").outerHeight() - window.innerHeight;
+var minheight = 0;
+
 $(document).ready(function(){
 	$('.charCarousel').slick({
 		dots: true,
@@ -8,8 +11,16 @@ $(document).ready(function(){
 	});
 });
 
-function go_to(target){
+function go_to(target, from){
+	maxheight += $(target).outerHeight();
+
 	$('html,body').animate({
 		scrollTop: $(target).offset().top},
 		'slow');
 }
+
+$(window).scroll(function(e){
+	if($(window).scrollTop() >= maxheight){
+		$(window).scrollTop(maxheight);
+	}
+})

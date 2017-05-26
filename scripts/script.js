e54d047073a3f0088d482a13c71f2019876a8f54
vhoyer <vhoyer@live.com>
2017-05-25 22:17:16 -0300

    Style to team/index, minor bug fixes, loading spin
    Signed-off-by: vhoyer <vhoyer@live.com>

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-2x fa-angle-left"></i></button>',
 		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-2x fa-angle-right"></i></button>',
 	});
+	$('.loading').remove();
 });
 
 function go_to(target, from){

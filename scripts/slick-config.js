fd7f80e7164b8cfc1644dc7c3d9fc8fc3d984326
vhoyer <vhoyer@live.com>
2017-06-06 21:28:11 -0300

    Added responsiveness to chars/index.html
    Signed-off-by: vhoyer <vhoyer@live.com>

--- scripts/slick-config.js
+++ scripts/slick-config.js
@@ -4,15 +4,8 @@ $(document).ready(function(){
 		speed: 500,
 		autoplay: true,
 		autoplaySpeed: 3000,
-		mobileFirst: true,
 		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-2x fa-angle-left"></i></button>',
 		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-2x fa-angle-right"></i></button>',
 	});
 	$('.loading').remove();
 });
-
-function go_to(target, from){
-	$('html,body').animate({
-		scrollTop: $(target).offset().top},
-		'slow');
-}
